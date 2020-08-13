6a201756f8cae1473ff5c1c7799fd54fac828a63
Eduardo <du_du.mba@hotmail.com>
2020-08-13 20:47:25 +0100

    update project

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,39 @@
-import { Router, request, response } from "express";
+import { Router } from "express";
 import { uuid } from "uuidv4";
+import { startOfHour, parseISO, isEqual } from "date-fns";
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+//adicionando tipagem
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
 
+  //analisa a data
+  const parsedDate = startOfHour(parseISO(date));
+
+  //procura agendamentos com horarios e datas iguais
+  const findAppointmentInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date),
+  );
+
+  if(findAppointmentInSameDate){
+    return response
+      .status(400)
+      .json({ message: "This appointment is already booked!" })
+  }
+
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
