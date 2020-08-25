033645324c0758527e59e50cae3039c456f91cf5
Eduardo <du_du.mba@hotmail.com>
2020-08-25 21:44:17 +0100

    update DB

--- src/models/Appointments.ts
+++ src/models/Appointments.ts
@@ -1,4 +1,6 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from "typeorm"; //Entity - Fazer ligação com o banco de dados. Column - definir abaixo qual o tipo de coluna.
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm"; //Entity - Fazer ligação com o banco de dados. Column - definir abaixo qual o tipo de coluna.
+
+import User from './User';
 
 @Entity('appointments') //Entity é uma função onde passa para o banco de dados tudo que esta na class abaixo.
 class Appointment {
@@ -7,6 +9,10 @@ class Appointment {
 
   @Column()
   provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
