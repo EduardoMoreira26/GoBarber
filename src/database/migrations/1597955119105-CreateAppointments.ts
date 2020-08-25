7893632c896ab1c334d3f3477dd6e2fe09b637a9
Eduardo <du_du.mba@hotmail.com>
2020-08-25 21:27:50 +0100

    update DB

--- src/database/migrations/1597955119105-CreateAppointments.ts
+++ src/database/migrations/1597955119105-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1597955119105 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
