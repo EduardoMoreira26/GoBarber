7803ac6900216fc899c6b200f15b684d97f07f7e
Eduardo <du_du.mba@hotmail.com>
2020-08-29 14:12:56 +0100

    update project

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,4 @@
-import { Request } from "express";
+import { getRepository } from 'typeorm';
 
 import User from '../models/User';
 
@@ -10,7 +10,25 @@ interface Request {
 
 class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
 
+    const checkUserExists = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Email address already used.')
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await userRepository.save(user);
+
+    return user;
   }
 
 }
