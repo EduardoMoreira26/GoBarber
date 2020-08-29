a219ccafc3da1a4f94164ff03da295d9692b2674
Eduardo <du_du.mba@hotmail.com>
2020-08-29 16:30:05 +0100

    create token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
-import { getRepository, InsertResult } from 'typeorm';
+import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
+
 
 import User from '../models/User';
 
@@ -10,6 +12,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +31,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.')
     }
 
+    const token = sign({}, '191f48250a93782bfc32998d1588eb35', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
 
   }
